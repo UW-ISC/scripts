9427cb2c417d9f9cf3e391a9ad6c6a82a8966bff
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-25 15:48:33 -0700

    Omit modification times on directories, silence the script and execute instead of echo.

--- media_rsync.sh
+++ media_rsync.sh
@@ -7,6 +7,5 @@ source $vars
 src_path="$REMOTE_USERNAME@$REMOTE_ADDRESS:$REMOTE_WORDPRESS_PATH/wp-content/uploads/"
 dest_path="$WORDPRESS_PATH/wp-content/uploads/"
 
-rsync_cmd="rsync -azv -e ssh $dest_path $src_path"
-echo "Currently this is just set to give the command to copy TO this host."
-echo $rsync_cmd
+rsync_cmd="rsync -azO -e ssh $src_path $dest_path"
+$rsync_cmd
