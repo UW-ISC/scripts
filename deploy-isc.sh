54b19e96c8fef05a7f6813e0942dc671884e79d0
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-27 14:36:11 -0700

    Add a script to deploy code on hosts to a dated branch from a pulled mainline branch.

---
+++ deploy-isc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source ./envvars.sh
+
+cd $WORDPRESS_PATH
+git checkout $TRACKING_BRANCH
+git pull
+git checkout -b `date "+%Y-%m-%d"` $TRACKING_BRANCH
+echo "Current branch is $TRACKING_BRANCH at: "
+git symbolic-ref HEAD
+
+cd $ISC_THEME_PATH
+git checkout $TRACKING_BRANCH
+git pull
+git checkout -b `date "+%Y-%m-%d"` $TRACKING_BRANCH
+echo "Current branch is $TRACKING_BRANCH at: "
+git symbolic-ref HEAD
