024953a5e08d9fe9b78eac7bcda50c9b0cdc3b76
Craig M. Stimmel <cstimmel@uw.edu>
2017-08-04 09:40:05 -0700

    Add hour and minute to branch name.

--- deploy-isc.sh
+++ deploy-isc.sh
@@ -5,13 +5,13 @@ source ./envvars.sh
 cd $WORDPRESS_PATH
 git checkout $TRACKING_BRANCH
 git pull
-git checkout -b `date "+%Y-%m-%d"` $TRACKING_BRANCH
+git checkout -b `date "+%Y-%m-%d-%H-%M"` $TRACKING_BRANCH
 echo "Current branch is $TRACKING_BRANCH at: "
 git symbolic-ref HEAD
 
 cd $ISC_THEME_PATH
 git checkout $TRACKING_BRANCH
 git pull
-git checkout -b `date "+%Y-%m-%d"` $TRACKING_BRANCH
+git checkout -b `date "+%Y-%m-%d-%H-%M"` $TRACKING_BRANCH
 echo "Current branch is $TRACKING_BRANCH at: "
 git symbolic-ref HEAD
